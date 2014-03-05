a8b48baed14ea9ade0e6aa20ccdf7a8da2147c2f
John Sellers <jrs9025@rit.edu>
2014-03-05 16:26:42 -0500

    Update Study.java

--- MedicalImaging/src/Study.java
+++ MedicalImaging/src/Study.java
@@ -1,7 +1,7 @@
 /*interface for the study classes
  * @author team3*/
 import java.util.Observable;
-public interface Study implements Observable {	
+public interface Study extends Observable {	
 	final int min=1;
 	final int max=4;
 	
