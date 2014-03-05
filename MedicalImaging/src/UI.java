bf56afdf958e0a37139646da7c2d333f71975efe
mumush <rhoff37@gmail.com>
2014-03-05 18:00:51 -0500

    More or less final GUI

--- MedicalImaging/src/UI.java
+++ MedicalImaging/src/UI.java
@@ -3,4 +3,10 @@ import java.util.Observer;
 
 public interface UI implements Observer {
 	
+	public void update(Observable arg0, Object arg1) {
+   
+      
+   
+   }
+   
 }
--- MedicalImaging/src/WorkStation.java
+++ MedicalImaging/src/WorkStation.java
@@ -7,21 +7,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Workstation implements UI {
+public class Workstation {
 
-	@Override
-	public void update(Observable arg0, Object arg1) {
-
-		
-
-	}
 
 	JFrame jf;
+   JMenuItem openItem;
 	JMenuItem aboutItem;
-	JTextField field;
-	Color origColor;
-	JButton enterBut;
-	JButton resetBut;
+   JMenuItem copyItem;
+   JMenuItem saveItem;
+   JMenuItem exitItem;
+   JMenuItem optionsItem;
+   
+   JButton previousButton;
+   JButton forwardButton;
+   
+   JRadioButton oneView;
+   JRadioButton fourView;
+   
+   ButtonGroup viewButtonGroup;
+   
 
    public Workstation() {
    
@@ -40,21 +44,21 @@ public class Workstation implements UI {
 		fileMenu.setMnemonic('F');
       
 		//create new JMenuItem for the File menu called Open
-		JMenuItem openItem = new JMenuItem("Open Study");
+		openItem = new JMenuItem("Open Study");
 		//set mnemonic for the exit option to the letter 'o'
 		openItem.setMnemonic('O');
 		//add the exitItem to the fileMenu
 		fileMenu.add(openItem);
       
 		//create new JMenuItem for the File menu called Copy
-		JMenuItem copyItem = new JMenuItem("Copy Study");
+		copyItem = new JMenuItem("Copy Study");
 		//set mnemonic for the exit option to the letter 'c'
 		copyItem.setMnemonic('C');
 		//add the exitItem to the fileMenu
 		fileMenu.add(copyItem);
       
 		//create new JMenuItem for the File menu called Save
-		JMenuItem saveItem = new JMenuItem("Save Study");
+		saveItem = new JMenuItem("Save Study");
 		//set mnemonic for the exit option to the letter 's'
 		saveItem.setMnemonic('S');
 		//add the exitItem to the fileMenu
@@ -67,41 +71,85 @@ public class Workstation implements UI {
 		//add the exitItem to the fileMenu
 		fileMenu.add(exitItem);
 		
-		//create new JMenu for Help
-		JMenu helpMenu = new JMenu("Help");
-		//set mnemonic for the help option to the letter 'h'
-		helpMenu.setMnemonic('H');
+		//create new JMenu for Preferences
+		JMenu prefsMenu = new JMenu("Preferences");
+		//set mnemonic for the help option to the letter 'r'
+		prefsMenu.setMnemonic('R');
 		
-		//create new JMenuItem for the Help menu called About
-		aboutItem = new JMenuItem("About");
+		//create new JMenuItem for the Preferences menu
+		optionsItem = new JMenuItem("Options");
 		//add the aboutItem to the helpMenu
-		helpMenu.add(aboutItem);
+		prefsMenu.add(optionsItem);
 		
 		//add the menus to the JMenuBar
 		jmb.add(fileMenu);
-		jmb.add(helpMenu);
+		jmb.add(prefsMenu);
 		
 		//add the jmb to the JFrame
 		jf.setJMenuBar(jmb);
       
+      //first row in grid layout
+      JPanel rowOne = new JPanel();
       
+      //create new image and image label
+      ImageIcon image = new ImageIcon("../studies/axial_head_mri/head01.jpg");
+      JLabel label = new JLabel("", image, JLabel.CENTER);
       
+      rowOne.add(label);
+      
+      ImageIcon image1 = new ImageIcon("../studies/axial_head_mri/head01.jpg");
+      JLabel label1 = new JLabel("", image1, JLabel.CENTER);
+      
+      rowOne.add(label1);
+      
+      ImageIcon image2 = new ImageIcon("../studies/axial_head_mri/head01.jpg");
+      JLabel label2 = new JLabel("", image2, JLabel.CENTER);
+      
+      rowOne.add(label2);
+      
+      ImageIcon image3 = new ImageIcon("../studies/axial_head_mri/head01.jpg");
+      JLabel label3 = new JLabel("", image3, JLabel.CENTER);
+      
+      rowOne.add(label3);
+      
+      //add row to JFrame
+      jf.add(rowOne);
+      
+      JPanel rowThree = new JPanel();
+      
+      previousButton = new JButton("Previous Image");
+      forwardButton = new JButton("Next Image");
+      
+      rowThree.add(previousButton);
+      rowThree.add(forwardButton);
+      
+      oneView = new JRadioButton("Single Image View");
+      oneView.setSelected(true);
+      fourView = new JRadioButton("Quad Image View");
+      
+      viewButtonGroup = new ButtonGroup();
+      viewButtonGroup.add(oneView);
+      viewButtonGroup.add(fourView);
+      
+      rowThree.add(oneView);
+      rowThree.add(fourView);
+      
+      jf.add(rowThree);
       
       
 		//******JFrame setup
 		
 		//set the title of the frame
-		jf.setTitle("Dat GUI");
+		jf.setTitle("Medical Image Workstation");
 		
 		//set the size of the frame
-		jf.setSize(230, 300);
+		jf.setSize(1100, 500);
 		
 		//set close operation
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//set the frame to be visible
 		jf.setVisible(true);
-		
 		
    
    }
